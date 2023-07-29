c77fd1256b075807072373eea9897668865c5deb
Tuan Nguyen <tuannguyen@Tuans-MacBook-Pro.local>
2023-07-28 23:15:48 -0400

    Update MUI icons

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,24 +1,47 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
 
 function CreateArea(props) {
   const [note, setNote] = useState({ title: "", content: "" });
+  const [isClick, setClick] = useState(false);
+
   function handleOnChange(event) {
     const { name, value } = event.target;
     return setNote((prevNote) => {
       return { ...prevNote, [name]: value };
     });
   }
+  function expandContent() {
+    setClick(true);
+  }
   function addNoteToList(event) {
+    setClick(false);
     props.addNote(note);
     setNote({ title: "", content: "" });
     event.preventDefault();
   }
+
   return (
-    <div>
+    <div onClick={expandContent}>
       <form>
         <input onChange={handleOnChange} name="title" value={note.title} placeholder="Title" />
-        <textarea onChange={handleOnChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
-        <button onClick={addNoteToList}>Add</button>
+        {isClick ? (
+          <>
+            <textarea
+              onChange={handleOnChange}
+              name="content"
+              value={note.content}
+              placeholder="Take a note..."
+              rows="3"
+            />
+            <div className="add-note">
+              <Fab onClick={addNoteToList}>
+                <AddIcon className="add-note" />
+              </Fab>
+            </div>
+          </>
+        ) : null}
       </form>
     </div>
   );
